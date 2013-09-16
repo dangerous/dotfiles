41e30613e8f3e73ddca1cce6261a601d31b5425c
David Rainsford <rainsford@acer.edu.au>
2013-09-16 12:33:09 +1000

    Unset properly when we say noproxy (for cucumber)

--- zsh/custom/proxy.zsh
+++ zsh/custom/proxy.zsh
@@ -1,5 +1,5 @@
 alias proxy='export http_proxy=http://192.168.1.248:3128;export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;'
-alias noproxy='export http_proxy='';export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;'
+alias noproxy='unset http_proxy;unset HTTPS_PROXY;unset HTTP_PROXY;unset FTP_PROXY;unset https_proxy;unset ftp_proxy'
 
 # run only if I am at work
 if [ `ifconfig | grep 10.10.0 | wc -l` = 1 ]; then
