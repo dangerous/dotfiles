a9a65b38a6efd696e1986c526e08029c3e781895
David Rainsford <david@rainsford.org>
2016-05-12 22:39:00 +1000

    Add no_proxy environment variable

--- zsh/custom/proxy.zsh
+++ zsh/custom/proxy.zsh
@@ -1,4 +1,4 @@
-alias proxy='export http_proxy=http://192.168.1.248:3128;export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;'
+alias proxy='export http_proxy=http://192.168.1.248:3128;export HTTPS_PROXY=$http_proxy;export HTTP_PROXY=$http_proxy;export FTP_PROXY=$http_proxy;export https_proxy=$http_proxy;export ftp_proxy=$http_proxy;export NO_PROXY="local-delivery,local-auth";export no_proxy=$NO_PROXY'
 alias noproxy='unset http_proxy;unset HTTPS_PROXY;unset HTTP_PROXY;unset FTP_PROXY;unset https_proxy;unset ftp_proxy'
 
 # run only if I am at work
